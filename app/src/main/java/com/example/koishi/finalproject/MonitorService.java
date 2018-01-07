b674321ed693aa65b36c4759491720781bd6b12b
Koishi <1440089139@qq.com>
2018-01-07 18:55:26 +0800

    second update

---
+++ app/src/main/java/com/example/koishi/finalproject/MonitorService.java
@@ -0,0 +1,62 @@
+package com.example.koishi.finalproject;
+
+import android.accessibilityservice.AccessibilityService;
+import android.app.Service;
+import android.app.admin.DevicePolicyManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.util.Calendar;
+
+/**
+ * Created by Koishi on 1/7/2018.
+ */
+
+public class MonitorService extends Service {
+    private int limitedHour;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        new MyThread().run();
+        super.onCreate();
+    }
+
+    private void readSetting() {
+        limitedHour = 18;
+    }
+
+    private void lockScreen() {
+        DevicePolicyManager devicePolicyManager = (DevicePolicyManager) getApplicationContext().getSystemService(Context.DEVICE_POLICY_SERVICE);
+        devicePolicyManager.lockNow();
+    }
+
+    private void sendSMS() {
+
+    }
+
+    private class MyThread extends Thread {
+        @Override
+        public void run() {
+            readSetting();
+            int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+            if (currentHour >= limitedHour) {
+                lockScreen();
+            }
+
+            if (currentHour == 22) {
+                sendSMS();
+            }
+            super.run();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/koishi/finalproject/MyDeviceAdminReceiver.java
@@ -0,0 +1,26 @@
+package com.example.koishi.finalproject;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.Calendar;
+
+/**
+ * Created by Koishi on 1/7/2018.
+ */
+
+public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent monitor = new Intent(context, MonitorService.class);
+            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, Calendar.getInstance().getTimeInMillis(), 1000 * 60 * 3, pendingIntent);
+        }
+        super.onReceive(context, intent);
+    }
+}
